a24b098b4c2a8500c9ab25baad8aa868c1aeac44
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-02 20:56:25 +0600

    5.1. usestate typing hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
 import './App.css';
 import User from './components/User';
 import Message from './components/Message';
 import Receptionist from './components/Receptionist';
 import NewUser from './components/NewUser';
+import Count from './components/Count';
 
 function App() {
-  // const [count, setCount] = useState<number>(0);
-
-  // const handleInc = () => {
-  //   setCount((count) => count + 1);
-  // };
-  // const handleDec = () => {
-  //   setCount((count) => count - 1);
-  // };
-  // const handleReset = () => {
-  //   setCount((count) => (count = 0));
-  // };
-
   // const salesMan1 = {
   //   name: 'jim halpert',
   //   email: 'jameshlapert2010s@yahoo.co.uk',
@@ -53,16 +41,13 @@ function App() {
 
   return (
     <div className="App">
-      {/* <h1> Count: {count} </h1>
-      <button onClick={handleInc}>Increment</button>
-      <button onClick={handleDec}>Decrement</button>
-      <button onClick={handleReset}>Reset</button> */}
+      <Count />
       {/* <h1>Children Props</h1>
       <Message text="UPDATE" />
       <User user={salesMan1} />
       <User user={salesMan2} />
       <Receptionist receptionists={receptionists} /> */}
-      <NewUser />
+      {/*   <NewUser />  */}
     </div>
   );
 }
--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Count = () => {
-  return <div></div>;
+  const [count, setCount] = useState<number>(0);
+
+  const handleInc = () => {
+    setCount((count) => count + 1);
+  };
+  const handleDec = () => {
+    setCount((count) => count - 1);
+  };
+  const handleReset = () => {
+    setCount((count) => (count = 0));
+  };
+  return (
+    <div>
+      <h1> Count: {count} </h1>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handleDec}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 };
 
 export default Count;
