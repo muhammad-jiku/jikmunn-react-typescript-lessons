8076b5978209a27f1f5bf8387b5ed48dc7c34d2d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-02 21:22:02 +0600

    8. usereducer event types

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,21 +1,91 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+const INCREMENT = 'INCREMENT';
+const INCREMENTBYAMOUNT = 'INCREMENTBYAMOUNT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const initialState = {
+  count: 0,
+};
+
+type counterState = {
+  count: number;
+};
+
+type IncrementActionType = {
+  type: typeof INCREMENT;
+};
+type IncrementByAmountActionType = {
+  type: typeof INCREMENTBYAMOUNT;
+  payload: number;
+};
+type DecrementActionType = {
+  type: typeof DECREMENT;
+};
+type ResetActionType = {
+  type: typeof RESET;
+};
+
+type counterActionType =
+  | IncrementActionType
+  | IncrementByAmountActionType
+  | DecrementActionType
+  | ResetActionType;
+
+const reducer = (state: counterState, action: counterActionType) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1,
+      };
+    case INCREMENTBYAMOUNT:
+      return {
+        count: state.count + action.payload,
+      };
+    case DECREMENT:
+      return {
+        count: state.count - 1,
+      };
+    case RESET:
+      return {
+        count: 0,
+      };
+
+    default:
+      throw new Error();
+  }
+};
 
 const Count = () => {
-  const [count, setCount] = useState<number>(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleInc = () => {
-    setCount((count) => count + 1);
+    dispatch({
+      type: INCREMENT,
+    });
+  };
+  const handleIncByAmount = () => {
+    dispatch({
+      type: INCREMENTBYAMOUNT,
+      payload: 5,
+    });
   };
   const handleDec = () => {
-    setCount((count) => count - 1);
+    dispatch({
+      type: DECREMENT,
+    });
   };
   const handleReset = () => {
-    setCount((count) => (count = 0));
+    dispatch({
+      type: RESET,
+    });
   };
   return (
     <div>
-      <h1> Count: {count} </h1>
+      <h1> Count : {state.count}</h1>
       <button onClick={handleInc}>Increment</button>
+      <button onClick={handleIncByAmount}>Increment by 5</button>
       <button onClick={handleDec}>Decrement</button>
       <button onClick={handleReset}>Reset</button>
     </div>
