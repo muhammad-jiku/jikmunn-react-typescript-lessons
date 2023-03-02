405e9b8bfdff5b98a7cdaedada1c3a9c9bfe18fe
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-02 20:49:48 +0600

    7. form events types

--- src/components/NewUser.tsx
+++ src/components/NewUser.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, MouseEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, MouseEvent, useState } from 'react';
 
 const NewUser = () => {
   const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
   const [status, setStatus] = useState<boolean>(false);
 
   const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -9,25 +10,50 @@ const NewUser = () => {
     setUsername(e.target.value);
   };
 
-  const handleUsernameClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    setStatus((prevStatus) => !prevStatus);
+    setEmail(e.target.value);
+  };
+
+  const handleDataClick = (e: FormEvent<HTMLFormElement>): object => {
+    e.preventDefault();
+    setStatus(true);
+    const newUser = { username, email };
+    console.log(newUser);
+    return newUser;
   };
   return (
     <div>
       <h1>Create New User</h1>
-      <input
-        type="text"
-        placeholder="Name"
-        value={username}
-        onChange={handleUsernameChange}
-      />
-      <button onClick={handleUsernameClick}>
-        {!status ? 'Show' : 'Hide'} name
-      </button>
+      <form onSubmit={handleDataClick}>
+        <div>
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email : </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
       {status ? (
         <>
           <h2>{username}</h2>
+          <h2>{email}</h2>
         </>
       ) : null}
     </div>
